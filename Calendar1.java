c91218baa91e2323b520ffe6905149ff7f9c97a8
Thadeo Arlo <arlo@air.local>
2024-01-11 09:32:14 +0200

    First Try

--- Calendar1.java
+++ Calendar1.java
@@ -2,38 +2,24 @@ public class Calendar1 {
 	static int dayOfMonth = 1;   
 	static int month = 1;
 	static int year = 1900;
-	static int dayOfWeek = 2;     // 1.1.1900 was a Monday
-	static int nDaysInMonth = 31; // Number of days in January
+	static int dayOfWeek = 2;   
+	static int nDaysInMonth = 31;
 	
-	/** 
-	 * Prints the calendars of all the years in the 20th century. Also prints the  
-	 * number of Sundays that occured on the first day of the month during this period.
-	 */
 	public static void main(String args[]) {
-		// Advances the date and the day-of-the-week from 1/1/1900 till 31/12/1999, inclusive.
-	    // Prints each date dd/mm/yyyy in a separate line. If the day is a Sunday, prints "Sunday".
-	    // The following variable, used for debugging purposes, counts how many days were advanced so far.
 	    int debugDaysCounter = 0; 
 		int SundayFirstDay = 0;
-	    //// Write the necessary initialization code, and replace the condition
-	    //// of the while loop with the necessary condition
 	 	while (year < 2000) {
 			if (dayOfWeek==1 && dayOfMonth==1) SundayFirstDay++;
 			if (dayOfWeek==1) {
 				System.out.println(dayOfMonth+"/"+month+"/"+year+" Sunday");
 			} else System.out.println(dayOfMonth+"/"+month+"/"+year);
 	 		advance();
-	 		//// If you want to stop the loop after n days, replace the condition of the
-	 		//// if statement with the condition (debugDaysCounter == n)
 	 		debugDaysCounter++;
-	 		if (debugDaysCounter == 5000) break;
+	 		// if (debugDaysCounter == 5000) break;
         }
 		System.out.println("During the 20th century, "+SundayFirstDay+" Sundays fell on the first day of the month");
 	 }
 	
-	 // Advances the date (day, month, year) and the day-of-the-week.
-	 // If the month changes, sets the number of days in this month.
-	 // Side effects: changes the static variables dayOfMonth, month, year, dayOfWeek, nDaysInMonth.
 	 private static void advance() {
 		dayOfMonth++;
 		dayOfWeek++;
