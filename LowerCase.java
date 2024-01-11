96f5d661dc8997bc6290fa96d6f27fd2ac14c38d
Thadeo Arlo <arlo@air.local>
2024-01-11 10:44:06 +0200

    First Try

--- LowerCase.java
+++ LowerCase.java
@@ -1,15 +1,9 @@
-/** String processing exercise 1. */
 public class LowerCase {
     public static void main(String[] args) {  
         String str = args[0];
         System.out.println(lowerCase(str));
     }
 
-   /**
-    * Returns a string which is identical to the original string, 
-    * except that all the upper-case letters are converted to lower-case letters.
-    * Non-letter characters are left as is.
-    */
     public static String lowerCase(String s) {
         String newString="";
         for (int i=0; i<s.length(); i++) {
