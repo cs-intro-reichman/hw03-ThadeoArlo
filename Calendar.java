3e87ffc6b17cb796687dc134123f78895307dc96
Thadeo Arlo <arlo@air.local>
2024-01-11 08:41:26 +0200

    First Try

--- Calendar.java
+++ Calendar.java
@@ -8,11 +8,13 @@ public class Calendar {
 	public static void main(String asdf[]) {
 		int yearIn = Integer.parseInt(asdf[0]);
 	 	while (year <= yearIn) {
-			if (dayOfWeek==1) {
-				System.out.println(dayOfMonth+"/"+month+"/"+year+" Sunday");
-			} else System.out.println(dayOfMonth+"/"+month+"/"+year);
+			if (year == yearIn) {
+				if (dayOfWeek==1) {
+					System.out.println(dayOfMonth+"/"+month+"/"+year+" Sunday");
+				} else System.out.println(dayOfMonth+"/"+month+"/"+year);
+			}
 	 		advance();
-        }
+		}
 	 }
 
 	 private static void advance() {
